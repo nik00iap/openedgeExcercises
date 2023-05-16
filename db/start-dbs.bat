1215bfd59aed10b75427292b711d538faf92147c
klaus.devries@iap.de <klaus.devries@iap.de>
2023-05-16 09:51:39 +0200

    new files

---
+++ db/start-dbs.bat
@@ -0,0 +1,4 @@
+SET DLC=d:\dlc\122.64bit
+%DLC%\bin\_mprosrv sports2000.db 	-H LOCALHOST -S 56132 -L 128000
+
+pause
---
+++ db/stop-dbs.bat
@@ -0,0 +1,6 @@
+SET DLC=d:\dlc\122.64bit
+REM Shutdown without question
+%DLC%\bin\_mprshut sports2000.db 	-by
+
+
+pause 
